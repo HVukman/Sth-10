f7ff9b040c93a924017ecb3de544190b635be80e
HVukman <peterslopp@googlemail.com>
2026-08-27 16:28:10 +0200

    changedstructure

---
+++ examples/bnuyy3.lua
@@ -0,0 +1,56 @@
+-- bnuyy3.lua
+local shapes_ = require("shapes")
+local color = require("colors")
+local draw_ = require("drawing")
+local window = require("window")
+local texture2 = require("texture2")
+local array = require("array")
+local text   = require("text")
+
+
+local limit = 100000
+local ax = array.new(limit)
+local ay = array.new(limit)
+
+
+local Text_
+local P1 = shapes_.newpoint(1, 1)
+
+local function init()
+
+    local width = 1280
+    local height = 640
+
+    local rand_ = math.random
+    for i = 1,(limit) do
+        ax[i] = rand_(width)
+        ay[i] = rand_(height)
+    end
+
+    window.title("Bnuuys")
+    window.set_window_position(200,100)
+    window.set_width_height(width,height)
+
+    Text_ = texture2.load_texture_handle("examples/resources/bunny.png")
+
+end
+
+local function draw()
+    draw_.clear_background(color.BLACK)
+    for i = 1, (limit) do
+        texture2.draw_texture_handle(Text_, ax[i], ay[i])
+    end
+    text.draw_fps(P1)
+end
+
+local function update()
+    local rand_ = math.random
+    for i = 1,(limit) do
+        ax[i] =  ax[i] + rand_(-1,1)
+        ay[i] =  ay[i] + rand_(-1,1)
+    end
+    draw()
+end
+
+
+return {init, update}
